21cea59c0f635460fd9fd4ce9a192c182a59ce9e
Aarav702-aps <rohit.tandon80@gmail.com>
2025-02-22 10:06:29 -0700

    L08 homework

---
+++ L08 homework.py
@@ -0,0 +1,13 @@
+a = int(input("enter value of a = "))
+b = int(input("enter value of b = "))
+c = int(input("enter value of c = "))
+d = a
+a = b
+b = c
+c = d
+
+print()
+print("after swapping value ")
+print("a = ",a)
+print("b = ",b)
+print("c = ",c)
