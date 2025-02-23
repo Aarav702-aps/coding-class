253311b4e96e21f0b77398233859059edd6ddfeb
Aarav702-aps <rohit.tandon80@gmail.com>
2025-02-23 10:46:27 -0700

    lesson 10

---
+++ L10 revese string.py
@@ -0,0 +1,7 @@
+word = input("please enter your string = ")
+reverse = ""
+
+for i in word:
+    reverse = i+reverse
+
+print("the reverse word = ",reverse)
---
+++ L10 sum of number.py
@@ -0,0 +1,7 @@
+a = int(input("enter the number you want to find the sum = "))
+sum = 0
+
+for i in range(1, a+1):
+    sum = sum + i
+
+print("sum = ",sum)
